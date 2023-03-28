8cb99788256c6ac2123cf997a655c0ff68751a97
HPCarey <hannahpcarey@gmail.com>
2023-03-28 17:00:25 +0000

    fix: LoggedInIcons variable typo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
 import Container from 'react-bootstrap/Container';
-import {Route,Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { createContext } from "react";
-
+import { createContext, useEffect, useState } from "react";
 
 export const CurrentUserContext = createContext();
-export const setCurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
 
   const handleMount = async () => {
-    try{
-      const {data} = await axios.get('dj_rest_auth/user/')
-      setCurrentUser(data)
-    } catch(err){
-
+    try {
+      const { data } = await axios.get('dj-rest-auth/user/');
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
-    handleMount()
-  }, [])
+    handleMount();
+  }, []);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
-      <setCurrentUserContext.Provider value={setCurrentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.Main}>
@@ -39,11 +37,11 @@ function App() {
               <Route exact path="/" render={() => <h1>Home Page</h1>} />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route render={()=><p>Page not found!</p>} />
+              <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
         </div>
-      </setCurrentUserContext.Provider>
+      </SetCurrentUserContext.Provider>
     </CurrentUserContext.Provider>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,40 +7,52 @@ import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
     const currentUser = useContext(CurrentUserContext);
-    const loggedInIcons = <>{currentUser}?.username</>
+    const loggedInIcons = <>{currentUser?.username}</>
     const loggedOutIcons = ( 
-    <>
-    <NavLink 
-    className={styles.NavLink} 
-    activeClassName={styles.Active} to="/signin">
-        <i className='fas fa-sign-in-alt'></i>
-        Sign In
-    </NavLink>
-    <NavLink  
-    className={styles.NavLink} 
-    activeClassName={styles.Active} 
-    to="/signup">
-        <i className='fas fa-user-plus'></i>
-        Sign Up
-    </NavLink>
-    </>
+        <>
+            <NavLink 
+                className={styles.NavLink} 
+                activeClassName={styles.Active}
+                to="/signin"
+            >
+                <i className='fas fa-sign-in-alt'></i>
+                    Sign In
+            </NavLink>
+            <NavLink  
+                className={styles.NavLink} 
+                activeClassName={styles.Active} 
+                to="/signup">
+                    <i className='fas fa-user-plus'></i>
+                    Sign Up
+            </NavLink>
+        </>
     );
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <NavLink to="/">
-                <Navbar.Brand href="#home"><img src={logo} alt="logo" height="45" /></Navbar.Brand>
+                    <Navbar.Brand href="#home">
+                        <img src={logo} alt="logo" height="45" />
+                    </Navbar.Brand>
                 </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/"><i className='fas fa-home'></i>Home</NavLink>
+                        <NavLink 
+                            exact 
+                            className={styles.NavLink} 
+                            activeClassName={styles.Active} 
+                            to="/"
+                            >
+                                <i className='fas fa-home'></i>Home
+                        </NavLink>
+
                         {currentUser ? loggedInIcons : loggedOutIcons}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
-}
+    );
+};
 
 export default NavBar;
