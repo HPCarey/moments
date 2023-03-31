c4e036344ea3bfe5828db197705858761c34475d
HPCarey <hannahpcarey@gmail.com>
2023-03-31 17:51:31 +0000

    add infinite scroll function in utils

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -12,6 +12,8 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import Asset from "../../components/Assets";
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
@@ -59,27 +61,33 @@ function PostsPage({ message, filter = "" }) {
         </Form>
         {hasLoaded ? (
           <>
-            {posts.results.length ? (
-              posts.results.map((post) => (
+          {posts.results.length ? (
+            <InfiniteScroll
+              children={posts.results.map((post) => (
                 <Post key={post.id} {...post} setPosts={setPosts} />
-              ))
-            ) : (
-              <Container className={appStyles.Content}>
-                <Asset src={NoResults} message={message} />
-              </Container>
-            )}
-          </>
-        ) : (
-          <Container className={appStyles.Content}>
-            <Asset spinner />
-          </Container>
-        )}
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
-      </Col>
-    </Row>
-  );
+              ))}
+              dataLength={posts.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!posts.next}
+              next={() => fetchMoreData(posts, setPosts)}
+            /> 
+          ) : (
+            <Container className={appStyles.Content}>
+              <Asset src={NoResults} message={message} />
+            </Container>
+          )}
+        </>
+      ) : (
+        <Container className={appStyles.Content}>
+          <Asset spinner />
+        </Container>
+      )}
+    </Col>
+    <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+      <p>Popular profiles for desktop</p>
+    </Col>
+  </Row>
+);
 }
 
 export default PostsPage;
