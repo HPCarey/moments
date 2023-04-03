c2d8287771753fe079b8967af2ef21b4783351b6
HPCarey <hannahpcarey@gmail.com>
2023-04-03 18:27:29 +0000

    create ProfileDataContext and ProfilePage

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,42 +1,12 @@
 import React from "react";
 import appStyles from "../../App.module.css";
 import { Container } from "react-bootstrap";
-import { useState } from "react";
-import { useEffect } from "react";
-import { axiosReq } from "../../api/axiosDefaults";
-import {
-  CurrentUserContext,
-  useCurrentUser,
-} from "../../contexts/CurrentUserContext";
 import Asset from "../../components/Assets";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 import Profile from "./Profile";
 
 const PopularProfiles = ({mobile}) => {
-  const [profileData, setProfileData] = useState({
-    // we will use the pageProfile later!
-    pageProfile: { results: [] },
-    popularProfiles: { results: [] },
-  });
-  const { popularProfiles } = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          popularProfiles: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+  const { popularProfiles } = useProfileData();
 
   return (
     <Container className={`${appStyles.Content} ${
@@ -63,6 +33,5 @@ const PopularProfiles = ({mobile}) => {
     </Container>
   );
 };
-
 
 export default PopularProfiles;
